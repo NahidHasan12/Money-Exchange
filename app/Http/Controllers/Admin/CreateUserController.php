81aa16448c818b6bfe021f22a0315542422d63e3
NahidHasan12 <nhd3456555@gmail.com>
2024-01-14 08:26:53 +0600

    money Exchange

--- app/Http/Controllers/Admin/CreateUserController.php
+++ app/Http/Controllers/Admin/CreateUserController.php
@@ -14,7 +14,7 @@ class CreateUserController extends Controller
    public function index(){
     return view('Admin.user_list');
    }
-//getdata
+//getdata users table
    public function getData(Request $request){
 
         if ($request->ajax()) {
